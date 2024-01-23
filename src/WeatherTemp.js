421ffe481e6c2bb722df3d245648b0ee1401240b
Jacintat30 <142204708+Jacintat30@users.noreply.github.com>
2024-01-23 12:30:46 -0600

    Made more changes to SRC

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,9 +1,12 @@
 import React, {useState} from react;
 export default function WeatherTemp(props) { 
+        let fahrenheit = (props.celsius * 9)/ 5 + 32;
         return (
             <div className="weatherTemp">
-                <span className="temp">{Math.round(fahrenheit)}</span>
-                <span className="unit">℉</span>
+                <span className="temp">{Math.round(props.fahrenheit)}</span>
+                <span className="unit">
+                  ℉
+                  </span>
                 </div>
                   );
 }
